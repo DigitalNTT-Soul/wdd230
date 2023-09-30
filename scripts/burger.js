533237c9194ebdc14ffea06fd8b54be477745890
Dylan Ruppell <dylan.ruppell@gmail.com>
2023-09-29 20:57:04 -0400

    First attempt at assignment "W03 Home Page Enhancement"

---
+++ scripts/burger.js
@@ -0,0 +1,8 @@
+const burger = document.querySelector('#burger');
+const navi = document.querySelector('.navi');
+
+burger?.addEventListener('click', () => {
+    navi?.classList.toggle('show');
+    burger?.classList.toggle('show');
+});
+
