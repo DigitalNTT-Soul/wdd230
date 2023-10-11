4ec796655ef171b2bf3e7cf731db4448b30102e6
Dylan Ruppell <dylan.ruppell@gmail.com>
2023-10-11 18:51:26 -0400

    My "Extra kick" for Step 4 of the W05 DOM Manipulation assignment was to allow the user to press enter while focused on the input box to mimic the clicking of the button.

--- lesson05/scripts/dom-manipulation.js
+++ lesson05/scripts/dom-manipulation.js
@@ -2,29 +2,35 @@ const list = document.querySelector('#list');
 const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
 
+// I despise anonymous and arrow functions I strongly prefer explicit functions
 button?.addEventListener('click', function() {
-    // I despise anonymous and arrow functions I strongly prefer explicit functions
-    if (input?.value == '') {
-        // According to most "never nesters", a gatekeepy "early return from negative
-        //  logic path" method is cleaner than having the main body of the code live
-        //  in blocks that are progressively more nested.
-        return false;
-    }
+    // According to most "never nesters", a gatekeepy "early return from negative
+    //  logic path" method is cleaner than having the main body of the code live
+    //  in blocks that are progressively more and more nested.
+    if (input?.value == '') return;
 
     const li = document.createElement('li');
-    li.innerHTML = input.value;
+    li.innerHTML = input?.value;
 
     const deleteButton = document.createElement('button');
     deleteButton.textContent = '❌';
 
     li.append(deleteButton);
-    list.append(li);
+    list?.append(li);
 
     deleteButton.addEventListener('click', function () {
-        list.removeChild(li);
-        input.focus();
+        list?.removeChild(li);
+        input?.focus();
     })
 
     input.value = '';
-    input.focus();
+    input?.focus();
 });
+
+input?.addEventListener('keypress', function(event) {
+    //  Another never-nester early return
+    if (event.key !== "Enter") return;
+
+    event.preventDefault();
+    button?.click();
+})
